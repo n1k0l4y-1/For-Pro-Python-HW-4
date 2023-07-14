c567c1aac78c3efd116b56f4b3b6bc34a149a184
Test <nikolay@MacBook-Air-Nikolaj.local>
2023-07-14 12:31:59 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,67 @@
+import types
+
+
+class FlatIterator:
+
+    def __init__(self, list_of_list):
+        self.list_of_list = iter(list_of_list)
+
+    def __iter__(self):
+        self.chunk = iter(next(self.list_of_list))
+        return self
+
+    def __next__(self):
+        try:
+            item = next(self.chunk)
+        except StopIteration:
+            try:
+                self.chunk = iter(next(self.list_of_list))
+                item = next(self.chunk)
+            except StopIteration:
+                raise StopIteration
+        return item
+
+
+def test_1():
+    list_of_lists_1 = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            FlatIterator(list_of_lists_1),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    ):
+        assert flat_iterator_item == check_item
+
+    assert list(FlatIterator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+
+
+def flat_generator(list_of_lists):
+    for i in list_of_lists:
+        for j in i:
+            yield j
+
+
+def test_2():
+    list_of_lists_1 = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            flat_generator(list_of_lists_1),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    ):
+        assert flat_iterator_item == check_item
+
+    assert list(flat_generator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+
+    assert isinstance(flat_generator(list_of_lists_1), types.GeneratorType)
+
+
+if __name__ == '__main__':
+    test_1()
+    test_2()
